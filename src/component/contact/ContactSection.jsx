b828e86b76491fdbe63a4333e7689b68d344b50c
Bikash Kumar Choudhury <kumarbikashchoudhury2004@gmail.com>
2025-01-24 01:58:52 +0530

    response change

--- src/component/contact/ContactSection.jsx
+++ src/component/contact/ContactSection.jsx
@@ -22,7 +22,7 @@ function ContactSection({ contactmeRef}) {
       <div className="grid grid-cols-1 lg:grid-cols-2 gap-8 lg:gap-16 items-center">
         <ContactForm />
         <div className="lg:w-3/4 ">
-          <div className="flex flex-col gap-5 lg:gap-9 w-[410px]">
+          <div className="flex flex-col gap-5 lg:gap-9 lg:w-[410px]">
             <p className="text-sm md:text-xl flex items-center gap-3">
               <MdAlternateEmail
                 className="bg-[#8b98a5] p-2 rounded-full hover:bg-[#16f2b3] hover:scale-110 transition-all duration-300 text-gray-800 cursor-pointer"
